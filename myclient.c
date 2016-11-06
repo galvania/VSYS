eb48a0f88cbee84a5600438fe80fc47ca7d9e072
Christoph Habres <christophhabres@hotmail.com>
2016-11-06 17:25:43 +0100

    Fresh start with Dennis in da pAST

---
+++ myclient.c
@@ -0,0 +1,113 @@
+/* myclient.c */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include "fileTransmissionHelper.h"
+
+#define FILE_TO_SEND "/home/schoko/Downloads/srv/"
+#define BUF 1024
+#define PORT 6543
+
+bool startsWith(const char *pre, const char *str)
+{
+    size_t lenpre = strlen(pre),
+           lenstr = strlen(str);
+    return lenstr < lenpre ? false : strncmp(pre, str, lenpre) == 0;
+}
+int main (int argc, char **argv)
+{
+    int create_socket;
+    char buffer[BUF];
+    struct sockaddr_in address;
+    int size;
+    char file_name[BUF];
+    char file_path[BUF];
+    char file_name_helper[BUF];
+    char file_size[256];
+    if( argc < 3 )
+    {
+        printf("Usage: %s ServerAdresse Verzeichnis\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if ((create_socket = socket (AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("Socket error");
+        return EXIT_FAILURE;
+    }
+
+    memset(&address,0,sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_port = htons (PORT);
+    inet_aton (argv[1], &address.sin_addr);
+
+    if (connect ( create_socket, (struct sockaddr *) &address, sizeof (address)) == 0)
+    {
+        printf ("Connection with server (%s) established\n", inet_ntoa (address.sin_addr));
+        size=recv(create_socket,buffer,BUF-1, 0);
+        if (size>0)
+        {
+            buffer[size]= '\0';
+            printf("%s",buffer);
+        }
+    }
+    else
+    {
+        perror("Connect error - no server available");
+        return EXIT_FAILURE;
+    }
+
+    do
+    {
+        int commandsize=strlen(buffer);
+        clrBuf(buffer);
+        printf ("Send message: ");
+        fgets (buffer, BUF, stdin);
+        send(create_socket, &commandsize, sizeof commandsize, 0);
+
+        send(create_socket, buffer, strlen (buffer), 0);
+        if((strcmp (buffer, "list\n") == 0))
+        {
+            clrBuf(buffer);
+            recv(create_socket,&commandsize,sizeof commandsize,0);
+
+            size = recv (create_socket, buffer, commandsize, 0);
+//            size=recv(create_socket,buffer,BUF-1, 0);
+            printf ("Files: \n%s", buffer);
+        }
+        else if (startsWith("get",buffer))
+        {
+            memset(file_name,'\0',sizeof file_name);
+            strncpy(file_name,&buffer[4],strlen(buffer)-4);
+            printf("fnf:%s \n",file_name);
+
+            strcpy(file_path,argv[2]);
+            file_name[strcspn(file_name, "\n")] = '\0';
+            printf("fna:%s \n",file_path);
+            recvFile(create_socket,file_name, file_path);
+        }
+        else if (startsWith("put",buffer))
+        {
+
+            memset(file_name,'\0',sizeof file_name);
+            strncpy(file_name,&buffer[4],strlen(buffer)-4);
+            printf("%s \n",file_name);
+
+            file_name[strcspn(file_name, "\n")] = '\0';
+            strcpy(file_name_helper,file_name);
+            strcpy(file_name,argv[2]);
+            strcat(file_name,file_name_helper);
+            printf("%s \n",file_name);
+            sendFile(create_socket,file_name);
+        }
+    }
+    while (strncmp(buffer, "quit",4) != 0);
+    close (create_socket);
+    return EXIT_SUCCESS;
+}
---
+++ myserver.c
@@ -0,0 +1,159 @@
+/* myserver.c */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "fileTransmissionHelper.h"
+#define BUF 1024
+#define PORT 6543
+#define FILE_TO_SEND    "/home/schoko/Downloads/srv/"
+
+int main (int argc, char **argv)
+{
+    int create_socket, new_socket;
+    socklen_t addrlen;
+    char buffer[BUF];
+    char listbuffer[BUF];
+    int size;
+    struct sockaddr_in address, cliaddress;
+
+    char file_name[BUF];
+    char file_name_helper[BUF];
+    char file_size[256];
+    int offset;
+    int remain_data;
+    char file_path[BUF];
+    int commandsize;
+    long port;
+    char * pEnd;
+    create_socket = socket (AF_INET, SOCK_STREAM, 0);
+if( argc < 2 )
+    {
+        printf("Usage: %s Directory Port\n ", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&address,0,sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    //Port wird in ein long gecastet und sollte ein ungueltiger Wert dafuer angegeben worden sein bricht das Programm ab.
+    port=strtol(argv[2],&pEnd,10);
+    if(port<1024 || port>65536){
+        perror("Invalid port. Port has to be in range of 1024-65536");
+        return EXIT_FAILURE;
+    }
+    if(*pEnd=='\0'){
+        perror("Not a valid port");
+    }
+    address.sin_port = htons (port);
+
+    if (bind ( create_socket, (struct sockaddr *) &address, sizeof (address)) != 0)
+    {
+        perror("bind error");
+        return EXIT_FAILURE;
+    }
+    listen (create_socket, 5);
+
+    addrlen = sizeof (struct sockaddr_in);
+
+    while (1)
+    {
+        printf("Waiting for connections...\n");
+        new_socket = accept ( create_socket, (struct sockaddr *) &cliaddress, &addrlen );
+        if (new_socket > 0)
+        {
+            printf ("Client connected from %s:%d...\n", inet_ntoa (cliaddress.sin_addr),ntohs(cliaddress.sin_port));
+            strcpy(buffer,"Welcome to myserver, Please enter your command:\n");
+            send(new_socket, buffer, strlen(buffer),0);
+        }
+        do
+        {
+
+            clrBuf(buffer);
+            recv(new_socket,&commandsize,sizeof commandsize,0);
+
+            size = recv (new_socket, buffer, commandsize, 0);
+            if( size > 0)
+            {
+//                buffer[size] = '\0';
+                printf ("Message received: %s\n", buffer);
+                if (strncmp(buffer, "list",4) == 0)
+                {
+                    DIR *dir;
+                    struct dirent *ent;
+                    printf("DIR %s T \n",argv[0]);
+                    if ((dir = opendir ("./")) != NULL)
+                    {
+//                        clrBuf(buffer);
+                        while ((ent = readdir (dir)) != NULL)
+                        {
+                            strcat(listbuffer,ent->d_name);
+                            strcat(listbuffer,"\n");
+                        }
+                        closedir (dir);
+                    }
+                    else
+                    {
+                        perror ("Fehler beim oeffnen des Ordners");
+                        return EXIT_FAILURE;
+                    }
+                    commandsize=strlen(listbuffer);
+                    send(new_socket, &commandsize, sizeof commandsize, 0);
+
+                    send(new_socket, listbuffer, commandsize, 0);
+
+                }
+                else if(strncmp(buffer, "get",3)  == 0)
+                {
+                    memset(file_name,'\0',sizeof file_name);
+                    strncpy(file_name,&buffer[4],strlen(buffer)-4);
+                    printf("%lu \n",strlen(file_name));
+
+                    file_name[strcspn(file_name, "\n")] = '\0';
+                    strcpy(file_name_helper,file_name);
+//                    clrBuf(file_name);
+
+                    strcpy(file_name,argv[1]);
+                    strcat(file_name,file_name_helper);
+                    printf("%s \n",file_name);
+                    sendFile(new_socket,file_name);
+                }
+                else if(strncmp(buffer, "put",3)  == 0)
+                {
+                    memset(file_name,'\0',sizeof file_name);
+                    strncpy(file_name,&buffer[4],strlen(buffer)-4);
+                    printf("%lu \n",strlen(file_name));
+
+                    file_name[strcspn(file_name, "\n")] = '\0';
+                    strcpy(file_path,argv[1]);
+                    printf("PUT-Filename:%s \n",file_name);
+                    printf("PUT-Filepath:%s \n",file_path);
+                    recvFile(new_socket,file_name,file_path);
+                }
+//                clrBuf(buffer);
+            }
+            else if (size == 0)
+            {
+                printf("Client closed remote socket\n");
+                break;
+            }
+            else
+            {
+                perror("recv error");
+                return EXIT_FAILURE;
+            }
+        }
+        while (strncmp (buffer, "quit", 4)  != 0);
+        close (new_socket);
+    }
+    close (create_socket);
+    return EXIT_SUCCESS;
+}
